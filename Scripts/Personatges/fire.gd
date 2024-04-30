c8f05ba1ebc0d608b9c289ee6b63a3a175cf4c85
Aran Estera <aranestera27@gmail.com>
2024-04-30 10:52:33 +0200

    Many changes
    -Blue tilemap
    -Players

--- Scripts/Personatges/fire.gd
+++ Scripts/Personatges/fire.gd
@@ -12,13 +12,22 @@ func _physics_process(delta):
 	# Add the gravity.
 	if gravity == true:
 		velocity.y = 600
+		scale = Vector2(4,4)
 	elif gravity == false:
 		velocity.y = -600
+		scale = Vector2(4,-4)
 
 	# Handle jump.
-	if Input.is_action_just_pressed("player_1_controller") and (is_on_ceiling() or is_on_floor()):
+	
+	if Input.is_action_just_pressed("player_fire") and (is_on_ceiling() or is_on_floor()):
 		gravity = !gravity
 	
 	velocity.x = 300
+	
+	#ANIMATION
+	
+	if is_on_ceiling() or is_on_floor():
+		$Animation.play("run")
+	else: $Animation.play("air")
 
 	move_and_slide()
