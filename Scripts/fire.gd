e81214d4e9f87c312f94b9d2c607f76763e37ce4
Aran Estera <aranestera27@gmail.com>
2024-05-06 12:06:16 +0200

    fire completed

--- Scripts/fire.gd
+++ Scripts/fire.gd
@@ -7,16 +7,26 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var change_gravity = true
 var gravity = true
+var invertit = false:
+	set(nova_inversio):
+		invertit = nova_inversio
+		if invertit:
+			$Animation.flip_v = true
+			$Normal.set_deferred("disabled", true)
+			$Invertida.set_deferred("disabled", false)
+		else:
+			$Animation.flip_v = false
+			$Normal.set_deferred("disabled", false)
+			$Invertida.set_deferred("disabled", true)
 
 func _physics_process(delta):
-	# Add the gravity.
+	
 	if gravity == true:
 		velocity.y = 600
-		scale = Vector2(4,4)
+		invertit = false
 	elif gravity == false:
 		velocity.y = -600
-		scale = Vector2(4,-4)
-
+		invertit = true
 	# Handle jump.
 	
 	if Input.is_action_just_pressed("player_fire") and (is_on_ceiling() or is_on_floor()):
