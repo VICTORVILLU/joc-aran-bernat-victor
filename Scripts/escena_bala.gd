6ce1a407a86053201433dba3d5b41ec1aec80b95
Aran Estera <aranestera27@gmail.com>
2024-05-16 09:44:13 +0200

    Bala gairabé acavada

--- Scripts/escena_bala.gd
+++ Scripts/escena_bala.gd
@@ -10,6 +10,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	$AnimatedSprite2D.play('air')
+	
 	var collision_info = move_and_collide(Velocitat.normalized() * delta * speed)
 	
 
+
+
+func _on_area_2d_body_entered(body):
+	if body.name in ["Laser","Laser2","Laser3","Laser4","Laser5","Laser6","Laser7","Laser8","Laser9", "Fire", "Fire_2", "bala_collision"]:
+		pass
+	else: 
+		print(body.name)
+		queue_free()
